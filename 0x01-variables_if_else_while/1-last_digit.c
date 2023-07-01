f55f11c37c2b542ff9540f48922f51af3c448fbf
NoorEldinMahmoud <Noor.mahmoudamin@gmail.com>
2023-07-01 16:20:55 -0700

    Last Digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,7 +17,7 @@ int main(void)
 	lastnum = n % 10;
 if (lastnum > 5)
 {
-printf("Last digit of  %d is %d and is grater than 5\n", n, lastnum);
+printf("Last digit of %d is %d and is grater than 5\n", n, lastnum);
 }
 else if (lastnum == 0)
 {
