1b1fad2cff93276e4cae3c9be6dd19e964c3d1e2
NoorEldinMahmoud <Noor.mahmoudamin@gmail.com>
2023-07-01 22:41:47 -0700

    Combinations of 4

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Combinations of 4 single digits
+ *
+ * Return: 0 Success.
+ */
+
+int main(void)
+{
+	int q, w;
+
+	for (q = 0; q <= 98; q++)
+	{
+		for (w = q + 1; w <= 98; w++)
+		{
+			putchar((q / 10) + '0');
+			putchar((q % 10) + '0');
+			putchar(' ');
+			putchar((w / 10) + '0');
+			putchar((w % 10) + '0');
+			if (q == 98 && w == 99)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}}
+	putchar('\n');
+	return (0);
+}
