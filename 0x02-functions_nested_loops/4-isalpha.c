11655f7fe1da387e6841ac5222e116791cdf2b1a
NoorEldinMahmoud <Noor.mahmoudamin@gmail.com>
2023-07-02 20:02:22 -0700

    Alphabatic characters

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isalpha - check for alphabatic charachter
+ * @c: The character to be checked
+ * Return: 1 for alphabatic and 0 for else.
+ */
+
+int _isalpha(int c)
+
+{
+
+	if ((c >= 65 && c<= 90) || (c >= 79 && c <= 122))
+	{
+		return (1);
+	}
+	return(0);
+}
